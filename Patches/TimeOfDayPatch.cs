b706573ca58cd2e35997248e15bf2e7a92795929
Lucas Frare <frare.lucas@outlook.com>
2025-01-19 08:26:35 -0300

    Add project

---
+++ Patches/TimeOfDayPatch.cs
@@ -0,0 +1,30 @@
+﻿using HarmonyLib;
+
+namespace QuotaCount.Patches;
+
+[HarmonyPatch(typeof(TimeOfDay))]
+internal static class TimeOfDayPatch
+{
+    [HarmonyPatch("UpdateProfitQuotaCurrentTime"), HarmonyPostfix]
+    internal static void UpdateProfitQuotaCurrentTimePostfix(ref TimeOfDay __instance)
+    {
+        QuotaCountBase.LogMessage(
+            $"Patching \"TimeOfDay UpdateProfitQuotaCurrentTime\"...",
+            BepInEx.Logging.LogLevel.Debug
+        );
+
+        StartOfRound.Instance.profitQuotaMonitorText.text = 
+            $"QUOTA {__instance.timesFulfilledQuota+1} PROFIT:\n${__instance.quotaFulfilled} / ${__instance.profitQuota}";
+
+        if (__instance.timeUntilDeadline <= 0f)
+        {
+            StartOfRound.Instance.deadlineMonitorText.text = $"QUOTA {__instance.timesFulfilledQuota+1} DEADLINE:\nNOW";
+        }
+        else
+        {
+            StartOfRound.Instance.deadlineMonitorText.text = $"QUOTA {__instance.timesFulfilledQuota+1} DEADLINE:\n{__instance.daysUntilDeadline} Days";
+        }
+
+        QuotaCountBase.LogMessage("Done!", BepInEx.Logging.LogLevel.Debug);
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,40 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace QuotaCount;
+
+[BepInPlugin(PLUGIN_GUID, PLUGIN_NAME, PLUGIN_VERSION)]
+public class QuotaCountBase : BaseUnityPlugin
+{
+    // plugin info
+    internal const string PLUGIN_GUID = "frare.QuotaCount";
+    internal const string PLUGIN_NAME = "Quota Count";
+    internal const string PLUGIN_VERSION = "1.0.0";
+
+    // singleton
+    internal static QuotaCountBase Instance;
+
+    // harmony instance
+    private readonly Harmony harmony = new Harmony(PLUGIN_GUID);
+
+    // for debugging
+    internal ManualLogSource logger;
+
+    private void Awake()
+    {
+        if (Instance == null) Instance = this;
+
+        logger = BepInEx.Logging.Logger.CreateLogSource(PLUGIN_GUID);
+        logger.LogInfo($"Mod started! :)");
+
+        harmony.PatchAll();
+
+        logger.LogInfo($"Mod finished loading!");
+    }
+
+    public static void LogMessage(string message, LogLevel logLevel = LogLevel.Debug)
+    {
+        Instance.logger.Log(logLevel, message);
+    }
+}
